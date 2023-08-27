5f1e69aee6eb4348d126914c41eb56ac2374b679
Jonathan Kim <vizard424@yahoo.com>
2023-08-26 18:07:56 -0600

    added conditions to vendor findall

--- routes/api/vendor-routes.js
+++ routes/api/vendor-routes.js
@@ -4,7 +4,13 @@ import Vendor from '../../models/Vendor.js';
 const router = Router();
 
 router.get('/', (req, res) => {
-    Vendor.findAll({})
+    Vendor.findAll({
+        attributes: [
+            'id',
+            'vendor_name',
+            'vendor_address'
+        ]
+    })
     .then(dbVendorData => res.json(dbVendorData))
     .catch(err => {
         console.log(err);
